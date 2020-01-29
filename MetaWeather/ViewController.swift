798ff9faf4853377f4b6c2fdc1dc6ae3f9d93c9c
Jongjin Seok <jinnsim@gmail.com>
2020-01-29 14:46:28 +0900

    - init project

---
+++ MetaWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetaWeather
+//
+//  Created by iOS_seok on 2020/01/29.
+//  Copyright © 2020 FP. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
